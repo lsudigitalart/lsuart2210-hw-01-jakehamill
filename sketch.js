5cdf1c390e7383f64653c3d0f082a68d101aa2e4
Jake Hamill <hamilljake1@gmail.com>
2016-08-28 17:56:40 -0500

    done

--- sketch.js
+++ sketch.js
@@ -3,7 +3,72 @@ function setup () {
   noFill(ellipse);
 }
 
-function draw() {
+
+  function draw() {
+
+    translate(200,-525);
+    ellipse(800,600,100,100);
+    ellipse(800,600,20,20);
+    strokeWeight(12);
+    ellipse(800,600,45,45);
+    translate(22,50);
+    ellipse(800,600,120,120);
+    ellipse(800,600,60,60);
+    strokeWeight(3);
+    ellipse(800,600,80,80);
+
+    ellipse(700,500,100,100);
+    ellipse(700,500,20,20);
+    strokeWeight(12);
+    ellipse(700,500,45,45);
+    translate(-50,-22);
+    ellipse(700,500,120,120);
+    ellipse(700,500,60,60);
+    strokeWeight(3);
+    ellipse(700,500,80,80);
+
+    ellipse(600,400,100,100);
+    ellipse(600,400,20,20);
+    strokeWeight(12);
+    ellipse(600,400,45,45);
+    translate(50,22);
+    ellipse(600,400,120,120);
+    ellipse(600,400,60,60);
+    strokeWeight(3);
+    ellipse(600,400,80,80);
+
+    ellipse(500,300,100,100);
+    ellipse(500,300,20,20);
+    strokeWeight(12);
+    ellipse(500,300,45,45);
+    translate(-22,-50);
+    ellipse(500,300,120,120);
+    ellipse(500,300,60,60);
+    strokeWeight(3);
+    ellipse(500,300,80,80);
+
+    ellipse(400,200,100,100);
+    ellipse(400,200,20,20);
+    strokeWeight(12);
+    ellipse(400,200,45,45);
+    translate(22,50);
+    ellipse(400,200,120,120);
+    ellipse(400,200,60,60);
+    strokeWeight(3);
+    ellipse(400,200,80,80);
+
+    ellipse(300,100,100,100);
+    ellipse(300,100,20,20);
+    strokeWeight(12);
+    ellipse(300,100,45,45);
+    translate(-50,-22);
+    ellipse(300,100,120,120);
+    ellipse(300,100,60,60);
+    strokeWeight(3);
+    ellipse(300,100,80,80);
+
+
+    translate(0,150)
     ellipse(800,600,100,100);
     ellipse(800,600,20,20);
     strokeWeight(12);
@@ -66,9 +131,374 @@ function draw() {
 
 
 
+    translate(0,150);
+    ellipse(800,600,100,100);
+    ellipse(800,600,20,20);
+    strokeWeight(12);
+    ellipse(800,600,45,45);
+    translate(22,50);
+    ellipse(800,600,120,120);
+    ellipse(800,600,60,60);
+    strokeWeight(3);
+    ellipse(800,600,80,80);
+
+    ellipse(700,500,100,100);
+    ellipse(700,500,20,20);
+    strokeWeight(12);
+    ellipse(700,500,45,45);
+    translate(-50,-22);
+    ellipse(700,500,120,120);
+    ellipse(700,500,60,60);
+    strokeWeight(3);
+    ellipse(700,500,80,80);
+
+    ellipse(600,400,100,100);
+    ellipse(600,400,20,20);
+    strokeWeight(12);
+    ellipse(600,400,45,45);
+    translate(50,22);
+    ellipse(600,400,120,120);
+    ellipse(600,400,60,60);
+    strokeWeight(3);
+    ellipse(600,400,80,80);
+
+    ellipse(500,300,100,100);
+    ellipse(500,300,20,20);
+    strokeWeight(12);
+    ellipse(500,300,45,45);
+    translate(-22,-50);
+    ellipse(500,300,120,120);
+    ellipse(500,300,60,60);
+    strokeWeight(3);
+    ellipse(500,300,80,80);
+
+    ellipse(400,200,100,100);
+    ellipse(400,200,20,20);
+    strokeWeight(12);
+    ellipse(400,200,45,45);
+    translate(22,50);
+    ellipse(400,200,120,120);
+    ellipse(400,200,60,60);
+    strokeWeight(3);
+    ellipse(400,200,80,80);
+
+    ellipse(300,100,100,100);
+    ellipse(300,100,20,20);
+    strokeWeight(12);
+    ellipse(300,100,45,45);
+    translate(-50,-22);
+    ellipse(300,100,120,120);
+    ellipse(300,100,60,60);
+    strokeWeight(3);
+    ellipse(300,100,80,80);
 
 
+    translate(0,150);
+    ellipse(800,600,100,100);
+    ellipse(800,600,20,20);
+    strokeWeight(12);
+    ellipse(800,600,45,45);
+    translate(22,50);
+    ellipse(800,600,120,120);
+    ellipse(800,600,60,60);
+    strokeWeight(3);
+    ellipse(800,600,80,80);
+
+    ellipse(700,500,100,100);
+    ellipse(700,500,20,20);
+    strokeWeight(12);
+    ellipse(700,500,45,45);
+    translate(-50,-22);
+    ellipse(700,500,120,120);
+    ellipse(700,500,60,60);
+    strokeWeight(3);
+    ellipse(700,500,80,80);
+
+    ellipse(600,400,100,100);
+    ellipse(600,400,20,20);
+    strokeWeight(12);
+    ellipse(600,400,45,45);
+    translate(50,22);
+    ellipse(600,400,120,120);
+    ellipse(600,400,60,60);
+    strokeWeight(3);
+    ellipse(600,400,80,80);
+
+    ellipse(500,300,100,100);
+    ellipse(500,300,20,20);
+    strokeWeight(12);
+    ellipse(500,300,45,45);
+    translate(-22,-50);
+    ellipse(500,300,120,120);
+    ellipse(500,300,60,60);
+    strokeWeight(3);
+    ellipse(500,300,80,80);
+
+    ellipse(400,200,100,100);
+    ellipse(400,200,20,20);
+    strokeWeight(12);
+    ellipse(400,200,45,45);
+    translate(22,50);
+    ellipse(400,200,120,120);
+    ellipse(400,200,60,60);
+    strokeWeight(3);
+    ellipse(400,200,80,80);
+
+    ellipse(300,100,100,100);
+    ellipse(300,100,20,20);
+    strokeWeight(12);
+    ellipse(300,100,45,45);
+    translate(-50,-22);
+    ellipse(300,100,120,120);
+    ellipse(300,100,60,60);
+    strokeWeight(3);
+    ellipse(300,100,80,80);
 
 
+    translate(0,150);
+    ellipse(800,600,100,100);
+    ellipse(800,600,20,20);
+    strokeWeight(12);
+    ellipse(800,600,45,45);
+    translate(22,50);
+    ellipse(800,600,120,120);
+    ellipse(800,600,60,60);
+    strokeWeight(3);
+    ellipse(800,600,80,80);
 
+    ellipse(700,500,100,100);
+    ellipse(700,500,20,20);
+    strokeWeight(12);
+    ellipse(700,500,45,45);
+    translate(-50,-22);
+    ellipse(700,500,120,120);
+    ellipse(700,500,60,60);
+    strokeWeight(3);
+    ellipse(700,500,80,80);
+
+    ellipse(600,400,100,100);
+    ellipse(600,400,20,20);
+    strokeWeight(12);
+    ellipse(600,400,45,45);
+    translate(50,22);
+    ellipse(600,400,120,120);
+    ellipse(600,400,60,60);
+    strokeWeight(3);
+    ellipse(600,400,80,80);
+
+    ellipse(500,300,100,100);
+    ellipse(500,300,20,20);
+    strokeWeight(12);
+    ellipse(500,300,45,45);
+    translate(-22,-50);
+    ellipse(500,300,120,120);
+    ellipse(500,300,60,60);
+    strokeWeight(3);
+    ellipse(500,300,80,80);
+
+    ellipse(400,200,100,100);
+    ellipse(400,200,20,20);
+    strokeWeight(12);
+    ellipse(400,200,45,45);
+    translate(22,50);
+    ellipse(400,200,120,120);
+    ellipse(400,200,60,60);
+    strokeWeight(3);
+    ellipse(400,200,80,80);
+
+    ellipse(300,100,100,100);
+    ellipse(300,100,20,20);
+    strokeWeight(12);
+    ellipse(300,100,45,45);
+    translate(-50,-22);
+    ellipse(300,100,120,120);
+    ellipse(300,100,60,60);
+    strokeWeight(3);
+    ellipse(300,100,80,80);
+
+
+    translate(0,150);
+    ellipse(800,600,100,100);
+    ellipse(800,600,20,20);
+    strokeWeight(12);
+    ellipse(800,600,45,45);
+    translate(22,50);
+    ellipse(800,600,120,120);
+    ellipse(800,600,60,60);
+    strokeWeight(3);
+    ellipse(800,600,80,80);
+
+    ellipse(700,500,100,100);
+    ellipse(700,500,20,20);
+    strokeWeight(12);
+    ellipse(700,500,45,45);
+    translate(-50,-22);
+    ellipse(700,500,120,120);
+    ellipse(700,500,60,60);
+    strokeWeight(3);
+    ellipse(700,500,80,80);
+
+    ellipse(600,400,100,100);
+    ellipse(600,400,20,20);
+    strokeWeight(12);
+    ellipse(600,400,45,45);
+    translate(50,22);
+    ellipse(600,400,120,120);
+    ellipse(600,400,60,60);
+    strokeWeight(3);
+    ellipse(600,400,80,80);
+
+    ellipse(500,300,100,100);
+    ellipse(500,300,20,20);
+    strokeWeight(12);
+    ellipse(500,300,45,45);
+    translate(-22,-50);
+    ellipse(500,300,120,120);
+    ellipse(500,300,60,60);
+    strokeWeight(3);
+    ellipse(500,300,80,80);
+
+    ellipse(400,200,100,100);
+    ellipse(400,200,20,20);
+    strokeWeight(12);
+    ellipse(400,200,45,45);
+    translate(22,50);
+    ellipse(400,200,120,120);
+    ellipse(400,200,60,60);
+    strokeWeight(3);
+    ellipse(400,200,80,80);
+
+    ellipse(300,100,100,100);
+    ellipse(300,100,20,20);
+    strokeWeight(12);
+    ellipse(300,100,45,45);
+    translate(-50,-22);
+    ellipse(300,100,120,120);
+    ellipse(300,100,60,60);
+    strokeWeight(3);
+    ellipse(300,100,80,80);
+
+
+    translate(0,150);
+    ellipse(800,600,100,100);
+    ellipse(800,600,20,20);
+    strokeWeight(12);
+    ellipse(800,600,45,45);
+    translate(22,50);
+    ellipse(800,600,120,120);
+    ellipse(800,600,60,60);
+    strokeWeight(3);
+    ellipse(800,600,80,80);
+
+    ellipse(700,500,100,100);
+    ellipse(700,500,20,20);
+    strokeWeight(12);
+    ellipse(700,500,45,45);
+    translate(-50,-22);
+    ellipse(700,500,120,120);
+    ellipse(700,500,60,60);
+    strokeWeight(3);
+    ellipse(700,500,80,80);
+
+    ellipse(600,400,100,100);
+    ellipse(600,400,20,20);
+    strokeWeight(12);
+    ellipse(600,400,45,45);
+    translate(50,22);
+    ellipse(600,400,120,120);
+    ellipse(600,400,60,60);
+    strokeWeight(3);
+    ellipse(600,400,80,80);
+
+    ellipse(500,300,100,100);
+    ellipse(500,300,20,20);
+    strokeWeight(12);
+    ellipse(500,300,45,45);
+    translate(-22,-50);
+    ellipse(500,300,120,120);
+    ellipse(500,300,60,60);
+    strokeWeight(3);
+    ellipse(500,300,80,80);
+
+    ellipse(400,200,100,100);
+    ellipse(400,200,20,20);
+    strokeWeight(12);
+    ellipse(400,200,45,45);
+    translate(22,50);
+    ellipse(400,200,120,120);
+    ellipse(400,200,60,60);
+    strokeWeight(3);
+    ellipse(400,200,80,80);
+
+    ellipse(300,100,100,100);
+    ellipse(300,100,20,20);
+    strokeWeight(12);
+    ellipse(300,100,45,45);
+    translate(-50,-22);
+    ellipse(300,100,120,120);
+    ellipse(300,100,60,60);
+    strokeWeight(3);
+    ellipse(300,100,80,80);
+
+
+    translate(0,150);
+    ellipse(800,600,100,100);
+    ellipse(800,600,20,20);
+    strokeWeight(12);
+    ellipse(800,600,45,45);
+    translate(22,50);
+    ellipse(800,600,120,120);
+    ellipse(800,600,60,60);
+    strokeWeight(3);
+    ellipse(800,600,80,80);
+
+    ellipse(700,500,100,100);
+    ellipse(700,500,20,20);
+    strokeWeight(12);
+    ellipse(700,500,45,45);
+    translate(-50,-22);
+    ellipse(700,500,120,120);
+    ellipse(700,500,60,60);
+    strokeWeight(3);
+    ellipse(700,500,80,80);
+
+    ellipse(600,400,100,100);
+    ellipse(600,400,20,20);
+    strokeWeight(12);
+    ellipse(600,400,45,45);
+    translate(50,22);
+    ellipse(600,400,120,120);
+    ellipse(600,400,60,60);
+    strokeWeight(3);
+    ellipse(600,400,80,80);
+
+    ellipse(500,300,100,100);
+    ellipse(500,300,20,20);
+    strokeWeight(12);
+    ellipse(500,300,45,45);
+    translate(-22,-50);
+    ellipse(500,300,120,120);
+    ellipse(500,300,60,60);
+    strokeWeight(3);
+    ellipse(500,300,80,80);
+
+    ellipse(400,200,100,100);
+    ellipse(400,200,20,20);
+    strokeWeight(12);
+    ellipse(400,200,45,45);
+    translate(22,50);
+    ellipse(400,200,120,120);
+    ellipse(400,200,60,60);
+    strokeWeight(3);
+    ellipse(400,200,80,80);
+
+    ellipse(300,100,100,100);
+    ellipse(300,100,20,20);
+    strokeWeight(12);
+    ellipse(300,100,45,45);
+    translate(-50,-22);
+    ellipse(300,100,120,120);
+    ellipse(300,100,60,60);
+    strokeWeight(3);
+    ellipse(300,100,80,80);
 }
